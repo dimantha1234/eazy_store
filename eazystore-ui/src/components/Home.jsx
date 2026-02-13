befe8459e730abd7c0d95a094017247a5e7bfb2f
dimantha1234 <dimanthathilakasiri97@gmail.com>
2026-02-14 01:15:43 +0530

    ui and backend

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,10 +1,13 @@
-import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient";
+import { useLoaderData } from "react-router-dom";
 
+// Hooks
 export default function Home() {
+  const products = useLoaderData();
   return (
-    <div className="home-container">
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
@@ -12,4 +15,16 @@ export default function Home() {
       <ProductListings products={products} />
     </div>
   );
+}
+
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get("/products"); // Axios GET Request
+    return response.data;
+  } catch (error) {
+    throw new Response(
+      error.message || "Failed to fetch products. Please try again.",
+      { status: error.status || 500 }
+    );
+  }
 }
